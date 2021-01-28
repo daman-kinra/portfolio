5d1c9172d57db14b93310cbb31c8acf1ef5689a4
Daman <daman.kinra@gmail.com>
2021-01-28 16:07:43 +0530

    About page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import LeftNavbar from "../Components/LeftNavbar";
 import "../Styles/Home.scss";
 import Download from "../Components/Download";
+import { FaLinkedinIn } from "react-icons/fa";
+import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
 
 function Home() {
   // eslint-disable-next-line
@@ -17,7 +19,54 @@ function Home() {
           <div className="heading">
             <h1>About</h1>
           </div>
-          <div className="content"></div>
+          <div className="socials">
+            <a
+              href="https://www.linkedin.com/in/daman-kinra"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="icons">
+                <FaLinkedinIn size="1rem" style={{ color: "556052" }} />
+                <p>LinkedIn</p>
+              </div>
+            </a>
+            <a
+              href="https://github.com/daman-kinra"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="icons">
+                <AiFillGithub size="1rem" style={{ color: "#556052" }} />
+                <p>Github</p>
+              </div>
+            </a>
+            <a
+              href="https://www.instagram.com/xcls00/?hl=en"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="icons">
+                <AiFillInstagram size="1rem" style={{ color: "#556052" }} />
+                <p>Instagram</p>
+              </div>
+            </a>
+          </div>
+          <div className="content">
+            <p style={{ fontSize: "15px" }}>
+              "I am an ordinary student with extra ordinary development skills
+              and exponential learning curve. Currently pursuing my B.tech
+              degree in computer science from Lovely Professional University. I
+              am doing my specialization in Full stack development and have done
+              many projects in MERN stack. I am always up to switch between the
+              technologies and learn new ones. I am expert in Frontend
+              development using ReactJs, really enjoy building UIs and
+              integrating it with NodeJs in the Backend. Apart from this I am an
+              active participator in academic events. I have experience working
+              in teams as I had participated in project building hackathons. I
+              also secured second position in All India Research Championship,
+              organized by Techkriti IIT Kanpur."
+            </p>
+          </div>
         </div>
       </section>
     </main>
